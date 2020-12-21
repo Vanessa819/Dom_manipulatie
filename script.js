0e592d255090136f1227fc1dfffea6f1fe3b4a1f
Vanessa819 <bellav2409@gmail.com>
2020-12-21 14:46:25 +0100

    Dom_manipulatie

---
+++ script.js
@@ -0,0 +1,41 @@
+//Deel 1
+console.log("test of deze javascript werkt");
+
+const animalSpotted = event => {
+  console.log("spotted:", event.target.textContent);
+  const spottedAnimal = event.target.textContent;
+  const listItem = document.createElement("li");
+  listItem.appendChild(document.createTextNode(spottedAnimal));
+  const list = document.getElementById("spotted-animals-list");
+  list.appendChild(listItem);
+};
+
+
+ //Deel 2
+
+ const removeFirstItem = () => {
+    const firstItem = document.getElementById("spotted-animals-list").firstChild;
+    firstItem.remove();
+  };
+ 
+  //Deel 3
+
+  const removeAll = () => {
+    const spottedAnimalsList = document.getElementById("spotted-animals-list");
+    spottedAnimalsList.innerHTML = "";
+  };
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const animalButtons = document.querySelectorAll(".big-five-button");
+    animalButtons.forEach(function(animalButton) {
+      animalButton.addEventListener("click", animalSpotted);
+    });
+  
+    const removeFirstItemButton = document.getElementById(
+      "remove-first-item-button"
+    );
+    removeFirstItemButton.addEventListener("click", removeFirstItem);
+  
+    const removeAllButton = document.getElementById("remove-all-button");
+    removeAllButton.addEventListener("click", removeAll);
+  });
